8dfe7e288ea7077ae80cade5c99883123d3ff1ed
David Aerne <meodai@gmail.com>
2016-09-04 20:52:52 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  #app
+    hello
+    .bla {{test}}
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data () {
+    return {
+      test: 'haha'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$c-black: #212121;
+$c-white: #fff;
+$bg: $c-white;
+$golden: 1.61803398875;
+$t-code: 'Inconsolata', ipm, Menlo, 'Courier New', monospace;
+
+body, html {
+  font-family: $t-code;
+  height: 100%;
+  font-size: calc(0.5rem + 1.4vh);
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App'
+import husl from 'husl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'html',
+  components: { App }
+})
+
+console.log(husl)
